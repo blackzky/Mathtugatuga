b3dd59e567036bf92e4614fee88da5f07bc93dbf
Gervel Giva <gervelgiva@gmail.com>
2013-09-23 14:54:39 +0800

    Added Unit Vector Calculator
    refactored code.
    added global setting for output decimal places

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 var AXIS = ['x', 'y' , 'z'];
+var DECIMAL_PLACES = 4;
 $(function(){
-	$("#vectors").show();
-	generateVectorInput();
+	show("vectors");
+	generateVectorInput("vector-magnitude", "Get Magnitude");
+	generateVectorInput("unit-vector", "Get Unit Vector");
 
-	$("body").on("change", "#vector-magnitude #components", function(){
-		generateVectorInput();
+	$("body").on("change", "#d_place", function(){
+		DECIMAL_PLACES = this.value;
 	});
+	$("body").on("change", "#vector-magnitude #components", function(){generateVectorInput("vector-magnitude", "Get Magnitude"); });
+	$("body").on("change", "#unit-vector #components", function(){generateVectorInput("unit-vector", "Get Unit Vector"); });
 });
 
 function show(id){
@@ -13,20 +17,65 @@ function show(id){
 	$("#" + id).show();
 }
 
-function generateVectorInput(){
-	var id ="vector-magnitude";
+function generateVectorInput(id, title){
 	$("#" + id + " #output").text('');
 	var components = $("#" + id + " #components").val();
 	var out = "Input: ";
 	for(var i = 0; i < components; i++){
-		out += "<input type='number' id='input" + i + "' class='input' value='0'/>" + AXIS[i] + " " + (i != components-1 ? "+" : "");
+		out += "<input type='number' id='input" + i + "' class='input' value='0'/>" + AXIS[i] + (i != components-1 ? " + " : "");
 	}
-	out += "<button onclick='vectorToMagnitude()'>Get Magnitude</button>";
+	out += " <button onclick='solve(\"" + id + "\")'>" + title  + "</button>";
 	$("#" + id + " #input").html(out);
 }
 
+function solve(mode){
+	switch(mode){
+		case "vector-magnitude":
+			vectorToMagnitude(mode);
+		break;
+		case "unit-vector":
+			solveUnitVector(mode);
+		break;
+		default:
+		alert("Nope");	
+		break;
+	}
+
+}
+
+function getInputValues(id){
+	var inputs = $("#" + id + " .input");
+	var components = inputs.size();
+	var input_values = [];
+	for(var i = 0; i < components; i++){
+		input_values.push(parseFloat(inputs[i].value));
+	}
+	return input_values;
+}
+
+function vectorToMagnitude(id){
+	var input_values = getInputValues(id);
+	var magnitude = getMagnitude(input_values);
+	$("#" + id + " #output").text("Magnitude: " + magnitude.toFixed(DECIMAL_PLACES));
+}
+
+function solveUnitVector(id){
+	var input_values = getInputValues(id);
+	var components = input_values.length;
+	var magnitude = getMagnitude(input_values);
+	var unit_vector = "";
+	var component = 0;
+	for(var i = 0; i < components; i++){
+		component = input_values[i]/magnitude;
+		unit_vector += (component > 0 ? (i == 0 ? "" : " + ") : " - ") + component.toFixed(DECIMAL_PLACES) + AXIS[i];
+	}
+	$("#" + id + " #output").text("Unit Vector: " + unit_vector);
+}
+
+
+/* Vector Operations */
+
 function getMagnitude(vector){
-	console.log(vector);
 	var components = vector.length;
 	var magnitude = 0;
 	var val = 0;
@@ -36,15 +85,4 @@ function getMagnitude(vector){
 	}
 	magnitude = Math.pow(magnitude, 1/2);
 	return magnitude;
-}
-function vectorToMagnitude(){
-	var id = "vector-magnitude";
-	var inputs = $("#" + id + " .input");
-	var components = inputs.size();
-	var input_values = [];
-	for(var i = 0; i < components; i++){
-		input_values.push(parseInt(inputs[i].value));
-	}
-	var magnitude = getMagnitude(input_values);
-	$("#" + id + " #output").text("Magnitude: " + magnitude.toFixed(4));
 }
