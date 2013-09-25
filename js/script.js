d6691d9cd57120210929cbcccced989eb88640ac
Gervel Giva <gervelgiva@gmail.com>
2013-09-26 00:44:18 +0800

    Version 1.02: Copy and Paste

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@
 	Description: A simple web application that applies the basic concepts of electromagnetics
 	Author: Gervel Giva
 */
-//a b c d e f g h i j k
-var AXIS = ['x', 'y' , 'z', 'v', 'u', 'w'];
 var VECTORS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 var DECIMAL_PLACES = 4;
 var BUFFER;
@@ -24,171 +22,87 @@ $(function(){
 });
 
 
-function solve(mode, operation){
-	switch(mode){
+function solve(id, operation){
+	var mag, vectorA, vectorB, vectorR; 
+	switch(id){
 		case "vector-magnitude":
-			vectorToMagnitude(mode);
+			vectorA = getVectorFromInputDom(id);
+			mag = Vector.Magnitude(vectorA, DECIMAL_PLACES);
+			$("#" + id + " .output").html("Magnitude: <strong>" + mag + "</strong>");
 			break;
 		case "unit-vector":
-			solveUnitVector(mode);
+			vectorA = getVectorFromInputDom(id);
+			vectorR = Vector.Unit(vectorA, DECIMAL_PLACES);
+			setVectorOutput(id, vectorR, "Unit Vector");
 			break;
 		case "vector-algebra":
+			vectorA = getVectorFromInputDom(id, 0);
+			vectorB = getVectorFromInputDom(id, 1);
 			if(operation == 0){
-				addVectors(mode);
+				vectorR = Vector.Add(vectorA, vectorB);
+				setVectorOutput(id, vectorR, "Sum");
 			}else if(operation == 1){
-				subVectors(mode);
+				vectorR = Vector.Sub(vectorA, vectorB);
+				setVectorOutput(id, vectorR, "Defference");
 			}else{
-				alert("Nope");	
+				alert("Something Went Wrong");	
 			}
 			break;
 		default:
-			alert("Nope");	
-			break;
+				alert("Something Went Wrong");	
+				break;
 	}
 
 }
 
-
-function addVectors(id){
-	var vectors = [];
-	var input_values = [];
-	var vector_inputs = $("#" + id + " #vector_num").val();
-	var components = $("#" + id + " #components").val();
-	for(var j = 0; j < vector_inputs; j++){
-		input_values = getInputValues(id + " #vector_input_" + VECTORS[j]);
-		vectors.push(input_values);
-	}
-	var vector_sum = [];
-	var out = "";
-	for(var i = 0; i < components; i++){
-		vector_sum.push(0);
-		for(var j = 0; j < vector_inputs; j++){
-			vector_sum[i] += vectors[j][i];
-		}
-		out += (vector_sum[i] >= 0 ? (i == 0 ? "" : " + ") : (i == 0 ? " -" : " - ")) + Math.abs(vector_sum[i].toFixed(DECIMAL_PLACES)) + AXIS[i];
-	}
-	$("#" + id + " #output").html("Vector Sum: <strong>" + out + "</strong>");
+function setVectorOutput(parentId, vectorR, title){
+	var out = (vectorR.x == "NaN" || vectorR.y == "NaN" || vectorR.z == "NaN") ?  "Error" : vectorR.toString();
+	var out = title + ": <strong>" + out + "</strong>";
+	out += "&nbsp;&nbsp;<button onclick='copy("+vectorR.x+", " + vectorR.y+ ", " + vectorR.z + ")' class='btn btn-info'>Copy</button>";
+	$("#" + parentId + " .output").html(out);
 }
-
-function subVectors(id){
-	var vectors = [];
-	var input_values = [];
-	var vector_inputs = $("#" + id + " #vector_num").val();
-	var components = $("#" + id + " #components").val();
-	for(var j = 0; j < vector_inputs; j++){
-		input_values = getInputValues(id + " #vector_input_" + VECTORS[j]);
-		vectors.push(input_values);
-	}
-	var vector_sum = [];
-	var out = "";
-
-	for(var i = 0; i < components; i++){
-		vector_sum.push(0);
-	}
-	vector_sum = vectors[0];
-	for(var i = 0; i < components; i++){
-		for(var j = 0; j < vector_inputs; j++){
-			if(j != 0) vector_sum[i] -= vectors[j][i];
-		}
-		out += (vector_sum[i] >= 0 ? (i == 0 ? "" : " + ") : (i == 0 ? " -" : " - ")) + Math.abs(vector_sum[i].toFixed(DECIMAL_PLACES)) + AXIS[i];
-	}
-	$("#" + id + " #output").html("Vector Difference: <strong>" + out + "</strong>");
+function copy(x, y, z){
+	BUFFER = new VectorData(x, y, z);
 }
-
-// out += "<input type='number' id='input" + i + "' class='input' value='0'/>" + AXIS[i] + (i != components-1 ? " + " : "");
-function generateVectors(id){
-	var out = "<br />";
-	var vectors = $("#" + id + " #vector_num").val();
-	var components = $("#" + id + " #components").val();
-	for(var j = 0; j < vectors; j++){
-		out += "<div id='vector_input_"+VECTORS[j]+"' class='row'><div class='input-group input-group-sm col-sm-1'><button class='btn disabled'>Vector " + VECTORS[j] + "</button></div>";
-		for(var i = 0; i < components; i++){
-			out += "<div class='input-group input-group-sm col-sm-2'><input type='number' id='input" + i + "' class='vector_input input form-control text-right' value='0'/><span class='input-group-addon'>" + AXIS[i] + "</span></div>";
-		}
-		out += "</div><br />";
-	}
-	out += " <button class='btn btn-primary' onclick='solve(\"" + id + "\", \"add\")'>Add Vectors</button>";
-	out += " <button class='btn btn-primary' onclick='solve(\"" + id + "\", \"sub\")'>Subtract Vectors</button>";
-
-	$("#" + id + " #vectors").html(out);
+function copyFromInputDom(parentId, index){
+	var vectorR = getVectorFromInputDom(parentId, index);
+	BUFFER = vectorR;
 }
-function getInputValues(id){
-	var inputs = $("#" + id + " .input");
-	var components = inputs.size();
-	var input_values = [];
-	for(var i = 0; i < components; i++){
-		input_values.push(parseFloat(inputs[i].value));
-	}
-	return input_values;
+function pasteInputDom(parentId, index){
+	setVectorOfInputDom(parentId, BUFFER, index);	
 }
-
-function vectorToMagnitude(id){
-	var input_values = getInputValues(id);
-	var magnitude = getMagnitude(input_values);
-	$("#" + id + " #output").html("Magnitude: <strong>" + magnitude.toFixed(DECIMAL_PLACES) + "</strong>");
-}
-
-function solveUnitVector(id){
-	var input_values = getInputValues(id);
-	var components = input_values.length;
-	var magnitude = getMagnitude(input_values);
-	var unit_vector = "";
-	var component = 0;
-	for(var i = 0; i < components; i++){
-		component = input_values[i]/magnitude;
-		unit_vector += (component >= 0 ? (i == 0 ? "" : " + ") : (i == 0 ? " -" : " - ")) + Math.abs(component.toFixed(DECIMAL_PLACES)) + AXIS[i];
-	}
-	$("#" + id + " #output").html("Unit Vector: <strong>" + unit_vector + "</strong>");
-}
-
-
-/* Vector Operations */
-
-function getMagnitude(vector){
-	var components = vector.length;
-	var magnitude = 0;
-	var val = 0;
-	for(var i = 0; i < components; i++){
-		val = Math.pow(vector[i], 2);
-		magnitude += val;
-	}
-	magnitude = Math.pow(magnitude, 1/2);
-	return magnitude;
-}
-
-/* REFACTORED CODE */
 function initModule(id, size, title){
-	for(var i = 0; i < size; i++){
-		createVectorInputDom(id);
-	}
+	createVectorInputDom(id, size);
 	createActionButton(id, title);
 }
-
 function createVectorInputDom(parentID, size){
-	var pdiv = "<div class='input-group input-group-sm col-sm-2'>";
+	var pdiv1 = "<div class='input-group input-group-sm col-sm-1'>";
+	var pdiv2 = "<div class='input-group input-group-sm col-sm-2'>";
 
 	var vector = "";
 	size = size || 1;
 	for(var i = 0; i < size; i++){
 		vector += "<br /><div class='row vector-input'>";
-		vector += (pdiv + "<input type='number' class='input form-control text-right vector-x' value='0'/><span class='input-group-addon'>x</span></div>");
-		vector += (pdiv + "<input type='number' class='input form-control text-right vector-y' value='0'/><span class='input-group-addon'>y</span></div>");
-		vector += (pdiv + "<input type='number' class='input form-control text-right vector-z' value='0'/><span class='input-group-addon'>z</span></div>");
+		
+		vector += (pdiv1 +"<div class='btn-group'><button type='button' class='btn btn-info dropdown-toggle' data-toggle='dropdown'>Vector " + VECTORS[i] + "&nbsp;<span class='caret'></span></button><ul class='dropdown-menu'><li><a href='javascript: copyFromInputDom(\""+parentID+"\", "+i+");'>Copy</a></li><li><a href='javascript: pasteInputDom(\""+parentID+"\", "+i+");'>Paste</a></li></ul></div></div>");
+ 
+
+		vector += (pdiv2 + "<input type='number' class='input form-control text-right vector-x' value='0'/><span class='input-group-addon'>x</span></div>");
+		vector += (pdiv2 + "<input type='number' class='input form-control text-right vector-y' value='0'/><span class='input-group-addon'>y</span></div>");
+		vector += (pdiv2 + "<input type='number' class='input form-control text-right vector-z' value='0'/><span class='input-group-addon'>z</span></div>");
 		vector += "</div>"; 
 	}
 
 	$("#" + parentID + " .vector-input-parent").append(vector);
 }
-
 function createActionButton(parentID, title){
 	var out = "<br /><div class='row'>";
 	for(var i in title){
-		out += "<div class='input-group input-group-sm col-sm-2'><button class='btn btn-primary' onclick='solve(\"" + parentID + "\", " + i + ")'>" + title[i]  + "</button></div>";	
+		out += "<div class='input-group input-group-sm col-sm-1'><button class='btn btn-primary' onclick='solve(\"" + parentID + "\", " + i + ")'>" + title[i]  + "</button></div>";	
 	}
 	out += "</div>";
 	$("#" + parentID + " .vector-input-parent").append(out);
 }
-
 function getVectorFromInputDom(domID, index){
 	var vectorDom = $("#" + domID + " .vector-input");
 	var vector = new VectorData();
@@ -198,7 +112,6 @@ function getVectorFromInputDom(domID, index){
 	vector.z = parseFloat( ($(vectorDom[index]).find(".vector-z")).val() ) || 0;
 	return vector;
 }
-
 function setVectorOfInputDom(domID, vector, index){
 	var vectorDom = $("#" + domID + " .vector-input");
 	index = index || 0;
